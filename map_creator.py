c3fc1cdd7c4f07a47d24b805bc94e5d6bb50f4e8
Victor Nossborn <vicno606@student.liu.se>
2022-04-26 15:15:38 +0200

    added node/edge labels and deletion on double click

--- map_creator.py
+++ map_creator.py
@@ -1,7 +1,8 @@
 from PySide6.QtCore import QLineF, QRectF, QSizeF, Qt
 from PySide6.QtGui import QPainter, QPen
 from PySide6.QtWidgets import (QApplication, QGraphicsItem, QGraphicsScene,
-                               QGraphicsView, QMainWindow)
+                               QGraphicsSceneMouseEvent, QGraphicsView,
+                               QMainWindow, QSizePolicy)
 
 from data import MapData
 
@@ -48,6 +49,21 @@ class Node(QGraphicsItem):
         self.edge_list.append(edge)
         edge.adjust()
 
+    def mouseDoubleClickEvent(self, event: QGraphicsSceneMouseEvent):
+        """ If user double clicks on two nodes, connect them """
+        last = self.graph.last_selected_node
+        if last is None:
+            print("Node {} selected".format(self.name))
+            self.graph.last_selected_node = self
+        else:
+            # TODO: Check that a edge doesn't already exist
+            print("Connected {}->{}".format(last.name, self.name))
+            self.graph.add_edge(last, self)
+            self.graph.last_selected_node = None
+
+        self.graph.scene().update()
+        return super().mouseDoubleClickEvent(event)
+
     def itemChange(self, change: QGraphicsItem.GraphicsItemChange, value):
         # Runs when this node has been changed
         if change == QGraphicsItem.GraphicsItemChange.ItemPositionHasChanged:
@@ -69,14 +85,14 @@ class Node(QGraphicsItem):
         painter.setPen(self.BORDER)
         painter.drawEllipse(-self.RADIUS/2, -self.RADIUS/2,
                             self.RADIUS, self.RADIUS)
+        painter.drawText(-4, 5, self.name)
 
 
 class Edge(QGraphicsItem):
     """ Represents an edge connecting two Nodes """
 
-    def __init__(self, start_node: Node, end_node: Node):
+    def __init__(self, start_node: Node, end_node: Node, weight: int = 1):
         super().__init__()
-        self.setAcceptedMouseButtons(Qt.NoButton)
         self.setZValue(-1)
 
         self.line = QLineF()
@@ -84,7 +100,7 @@ class Edge(QGraphicsItem):
         self.end = end_node
 
         self.name = start_node.name + end_node.name  # Combine both node names
-        self.weight = 1  # Default weight
+        self.weight = weight
 
         self.start.addEdge(self)
         self.end.addEdge(self)
@@ -99,44 +115,68 @@ class Edge(QGraphicsItem):
 
         return self.start if node == self.end else self.end
 
+    def delete(self):
+        """ Disconnect edge from nodes and remove from scene """
+        if self.start is not None:
+            self.start.disconnect_edge(self)
+        if self.end is not None:
+            self.end.disconnect_edge(self)
+
+        super().scene().removeItem(self)
+
     def adjust(self):
         self.prepareGeometryChange()
 
         self.line.setP1(self.start.pos())
         self.line.setP2(self.end.pos())
 
-    def boundingRect(self):
-        pen_width = 5
-        extra = (pen_width + 10) / 2.0
+    def mouseDoubleClickEvent(self, event: QGraphicsSceneMouseEvent):
+        """ If user double clicks an edge, delete it """
+        print("Deleting edge " + self.name)
+        self.delete()
 
+        return super().mouseDoubleClickEvent(event)
+
+    def boundingRect(self):
         start = self.line.p1()
         end = self.line.p2()
 
         rect = QRectF(start, QSizeF(end.x() - start.x(), end.y() - start.y()))
-        rect = rect.normalized().adjusted(-extra, -extra, -extra, -extra)
-        return rect
+        return rect.normalized()
 
     def paint(self, painter: QPainter, _option, _widget):
         painter.setPen(QPen(Qt.black, 5, Qt.SolidLine,
                        Qt.RoundCap, Qt.RoundJoin))
         painter.drawLine(self.line)
 
+        # Calculate where to put name label
+        center_ptn = self.line.center()
+        offset_x = self.line.normalVector().dx() / self.line.length() * 15
+        offset_y = self.line.normalVector().dy() / self.line.length() * 15
+
+        painter.drawText(center_ptn.x() + offset_x, center_ptn.y() + offset_y,
+                         "{}: {}".format(self.name, self.weight))
+
 
 class MapCreatorWidget(QGraphicsView):
-    """ A widget for creating maps """
+    """ A widget for creating maps, represented as an undrirected weighted graph """
 
     WINDOW_SIZE = 800
 
+    # Remeber which node was clicked on last
+    last_selected_node: Node = None
+
     def __init__(self):
         super().__init__()
-        self.setMaximumSize(self.WINDOW_SIZE, self.WINDOW_SIZE)
+        self.setMinimumSize(self.WINDOW_SIZE, self.WINDOW_SIZE)
+        self.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
         self.setWindowTitle("Kartritning")
 
         scene = QGraphicsScene()
 
         scene.setItemIndexMethod(QGraphicsScene.NoIndex)
-        scene.setSceneRect(-self.width()/2, -self.width()/2,
-                           self.width(), self.width())
+        scene.setSceneRect(-self.WINDOW_SIZE/2, -self.WINDOW_SIZE/2,
+                           self.WINDOW_SIZE, self.WINDOW_SIZE)
         self.setScene(scene)
 
         # self.setCacheMode(QGraphicsView.CacheBackground)
@@ -157,12 +197,12 @@ class MapCreatorWidget(QGraphicsView):
         for node in self.nodes:
             scene.addItem(node)
 
-        scene.addItem(Edge(self.nodes[0], self.nodes[1]))
+        scene.addItem(Edge(self.nodes[0], self.nodes[1], 0))
         scene.addItem(Edge(self.nodes[1], self.nodes[8]))
         scene.addItem(Edge(self.nodes[2], self.nodes[4]))
         scene.addItem(Edge(self.nodes[3], self.nodes[8]))
         scene.addItem(Edge(self.nodes[8], self.nodes[4]))
-        scene.addItem(Edge(self.nodes[8], self.nodes[6]))
+        scene.addItem(Edge(self.nodes[8], self.nodes[6], 0))
         scene.addItem(Edge(self.nodes[4], self.nodes[7]))
         scene.addItem(Edge(self.nodes[6], self.nodes[5]))
         scene.addItem(Edge(self.nodes[7], self.nodes[6]))
@@ -177,6 +217,11 @@ class MapCreatorWidget(QGraphicsView):
         self.nodes[5].setPos(-space_const, space_const)
         self.nodes[6].setPos(0, space_const)
         self.nodes[7].setPos(space_const, space_const)
+
+    def add_edge(self, node1, node2):
+        """ Connect two nodes with an edge """
+        edge = Edge(node1, node2)
+        self.scene().addItem(edge)
 
     def delete_node(self, node: Node):
         """ Deletes node from widget """
